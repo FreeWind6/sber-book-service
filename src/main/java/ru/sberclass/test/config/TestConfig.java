50dce8e139ce912157da35f535e33e4ae98f73fc
Dmitry Kubyshkin <free-wind-2012@yandex.ru>
2023-03-17 18:09:44 +0300

    Test

---
+++ src/main/java/ru/sberclass/test/config/TestConfig.java
@@ -0,0 +1,8 @@
+package ru.sberclass.test.config;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+}
