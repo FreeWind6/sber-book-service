6143f419ce92e02213e9c861ca24b76e5f11a410
Dmitry <free-wind-2012@yandex.ru>
2023-03-17 18:12:08 +0300

    Test (#1)
    * Test
    
    * Test
    
    * Test

---
+++ src/main/java/ru/sberclass/test/config/TestConfig.java
@@ -0,0 +1,10 @@
+package ru.sberclass.test.config;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+    //TODO
+    //sadasd
+}
